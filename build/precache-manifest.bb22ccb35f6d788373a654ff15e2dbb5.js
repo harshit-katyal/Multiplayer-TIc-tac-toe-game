8a48276da5189adc3b8fb20772aa3de3870bf5f7
harshit-katyal <hkatyal345@gmail.com>
2022-03-30 10:25:56 +0530

    build folder push

---
+++ build/precache-manifest.bb22ccb35f6d788373a654ff15e2dbb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6470773e700996b807eee3fb3e19834a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9f181999ff217cf338",
+    "url": "/static/css/main.4b773ba4.chunk.css"
+  },
+  {
+    "revision": "656e2afcfad2315c6e3e",
+    "url": "/static/js/2.edd8bbe1.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.edd8bbe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9f181999ff217cf338",
+    "url": "/static/js/main.260a1149.chunk.js"
+  },
+  {
+    "revision": "828c0bdfa56a628b17a7",
+    "url": "/static/js/runtime-main.eb9be228.js"
+  },
+  {
+    "revision": "ef02fd95b5c1f835dcbb42e16b024bbd",
+    "url": "/static/media/logo.ef02fd95.png"
+  }
+]);
